bbd4fe968fd212006d443c6911fe8beddc3e5488
Lukas Kraehling <l.kraehling@protonmail.com>
2022-10-23 05:04:01 +0200

    Updated Version-String

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "2.0.9|DW7.4.7|LK7"
+#define SHORT_BUILD_VERSION "2.0.9+|DW7.4.7"
 
 /**
  * Verbose version identifier which should contain a reference to the location
